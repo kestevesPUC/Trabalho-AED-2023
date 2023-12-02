506906730fa583eb59874a27e1e9b69e07042e5e
Kaio Gomes <kesteves.dev@gmail.com>
2023-12-02 09:13:45 -0300

    classe curso e classe candidato

---
+++ Curso.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TRAB_AED_2023
+{
+
+    internal class Curso
+    {
+        private string codigo {  get; set; }
+        private string nome { get; set; }
+        private int qtdVagas { get; set; }
+        private static double notaDeCorte;
+
+    }
+}
